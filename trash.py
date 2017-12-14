60f2a3d42117071c2f399e02dd3983bbd43e8eda
Nurtal <nathan.foulquier.pro@gmail.com>
2017-12-14 18:22:04 +0100

    population initialisation

--- trash.py
+++ trash.py
@@ -272,6 +272,220 @@ def simple_conversion_to_img_matrix(data_file):
 
 
 
+
+
+def init_grid_matrix(corr_mat):
+	##
+	## Just a function to instanciate
+	## randomly a grid matrix
+	##
+
+	number_of_variables = len(corr_mat[0])
+	side_size = math.sqrt(number_of_variables)
+	side_size = int(side_size)
+
+	## Randomy assign a position in the grid to a variable
+	variable_to_position = {}
+	for x in xrange(0,number_of_variables):
+		
+		position_assigned = False
+
+		while(not position_assigned):
+			x_position = random.randint(0,side_size-1)
+			y_position = random.randint(0,side_size-1)
+			position = [x_position, y_position]
+
+			if(position not in variable_to_position.values()):
+				variable_to_position[x] = position
+				position_assigned = True
+
+	## Write a matrix with the assigned position as coordinates
+	## for the variables
+	
+	## init the matrix
+	## [WARNINGS] => Still assume we deal
+	## with a square matrix / image for the representation
+	## of the patient
+	
+	## Init matrix
+	map_matrix = numpy.zeros(shape=(side_size,side_size))
+
+	## Fill the matrix
+	for variable in variable_to_position.keys():
+		position = variable_to_position[variable]
+		x_position = position[0]
+		y_position = position[1]
+
+		map_matrix[x_position][y_position] = variable
+
+
+	return map_matrix
+
+
+
+def get_neighbour(var, grid_matrix):
+	##
+	## get the list of neighbour of var in grid_matrix
+	##
+
+	## locate var in grid
+	x_position = -1
+	y_position = -1
+
+	row = 0
+	for x in grid_matrix:
+		column = 0
+		for y in x:
+			if(y == var):
+				x_position = row
+				y_position = column
+			column += 1
+		row += 1
+
+	## Get the neighbour	
+	neighbours = []
+
+	if(y_position > 0):
+		n_x_position = x_position
+		n_y_position = y_position - 1
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+	if(y_position < len(grid_matrix[0])-1):
+		n_x_position = x_position
+		n_y_position = y_position + 1
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+	if(x_position > 0):
+		n_x_position = x_position - 1
+		n_y_position = y_position
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+	if(x_position < len(grid_matrix)-1):
+		n_x_position = x_position + 1
+		n_y_position = y_position
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+
+	if(y_position > 0 and x_position > 0):
+		n_x_position = x_position - 1
+		n_y_position = y_position - 1
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+	if(y_position > 0 and x_position < len(grid_matrix)-1):
+		n_x_position = x_position + 1
+		n_y_position = y_position - 1
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+	if(y_position < len(grid_matrix[0])-1 and x_position > 0):
+		n_x_position = x_position - 1
+		n_y_position = y_position + 1
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+	if(y_position < len(grid_matrix[0]) - 1 and x_position < len(grid_matrix) - 1):
+		n_x_position = x_position + 1
+		n_y_position = y_position + 1
+		n = grid_matrix[n_x_position][n_y_position]
+		neighbours.append(n)
+
+	## return the list of neighbours
+	return  neighbours
+
+
+
+def compute_matrix_score(corr_matrix, grid_matrix):
+	##
+	## Score is the sum of distance between each element of the grid
+	## and its neighbour 
+	##
+
+	## absolute value
+	corr_matrix = abs(corr_matrix)
+
+	total_score = 0.0
+
+	## for each pixel:
+	for vector in grid_matrix:
+		for scalar in vector:
+
+			## get neighbour
+			neighbours = get_neighbour(scalar, grid_matrix)
+
+			## compute score for each pixel (the distance from each variable with their neighbours)
+			for n in neighbours:
+				total_score += corr_matrix[int(scalar)][int(n)]
+
+
+	## return global score
+	return total_score
+
+
+
+import operator
+
+
+
+
+
+
+def select_parents(population, good_parents, bad_parents, dist_matrix):
+	##
+	## Select parents from population
+	## - population is a list of matrix
+	## - good_parents is an int, number of good (best score)
+	##   parents to return
+	## - bad parents is an int, number of bad (low score)
+	##   parents to return
+	##
+
+	## Compute the score for each individual (matrix) in the
+	## population.
+	ind_index_to_score = {}
+	ind_index = 0
+	for ind in population:
+		ind_index_to_score[ind_index] = compute_matrix_score(dist_matrix, ind)
+		ind_index += 1
+
+
+	## Select good parents, i.e the top score
+	all_good_parents_assigned = False
+	number_of_good_parents = 0
+	list_of_good_parents = []
+	while(not all_good_parents_assigned):
+
+		selected_parent = max(ind_index_to_score.iteritems(), key=operator.itemgetter(1))[0]
+		del ind_index_to_score[selected_parent]
+		list_of_good_parents.append(selected_parent)
+		number_of_good_parents += 1
+
+		if(number_of_good_parents == good_parents):
+			all_good_parents_assigned = True
+
+	## Select bad parents, i.e the low score
+	all_bad_parents_assigned = False
+	number_of_bad_parents = 0
+	list_of_bad_parents = []
+	while(not all_bad_parents_assigned):
+
+		selected_parent = min(ind_index_to_score.iteritems(), key=operator.itemgetter(1))[0]
+		del ind_index_to_score[selected_parent]
+		list_of_bad_parents.append(selected_parent)
+		number_of_bad_parents += 1
+
+		if(number_of_bad_parents == bad_parents):
+			all_bad_parents_assigned = True
+
+
+	## Create the list of patient to return
+	parents_id = list_of_good_parents + list_of_bad_parents
+	parents = []
+	for p_id in parents_id:
+		parents.append(population[p_id])
+
+	return parents
+
+
+
+
 def build_image_map(data_file):
 	##
 	## [IN PROGRESS]
@@ -314,7 +528,7 @@ def build_image_map(data_file):
 		best_neighbours_in_vector = best_neighbours_in_vector[numpy.argsort(vector[best_neighbours_in_vector])]
 		best_neighbours_in_vector = list(best_neighbours_in_vector)
 		best_neighbours_in_vector.remove(variable_id)
-		best_neighbours_in_vector.reverse()		
+		best_neighbours_in_vector.reverse()
 		variable_id_to_near_variables[variable_id] = best_neighbours_in_vector
 		variable_id += 1
 
@@ -377,10 +591,32 @@ def build_image_map(data_file):
 
 		map_matrix[x_position][y_position] = variable
 
+	## -> Compute matrix score
+	initial_score = compute_matrix_score(dist_mat, map_matrix)
 
 
+	##------------------------##
+	## LEARN THE OPTIMAL GRID ##
+	##------------------------##
+	##
+	## Create a population of random grid and then
+	## use a genetic algorithm to learn the optimal grid
+	##
 
+	## Create initial population
+	initial_population = []
+	for x in xrange(0,300):
+		random_grid = init_grid_matrix(dist_mat)
+		initial_population.append(random_grid)
 
+	## Select parents
+	parents = select_parents(initial_population, 20,5, dist_mat)
+
+	## [TODO]
+	## Crossing over
+	## The idea is to generate a lot of matrix from the parents
+	## and destroy the non-conventional matrix after (i.e matrix
+	## with plusieurs fois the same variable)
 
 
 	## Get new Ideas 
@@ -391,9 +627,15 @@ def build_image_map(data_file):
 ### TEST SPACE ###
 generate_random_data(36	,85)
 corr_mat = get_correlation_matrix("trash_data.csv")
+
 create_image_from_csv("trash_data.csv", "machin.png")
 normalize_data("trash_data.csv")
 simple_conversion_to_img_matrix("trash_data_scaled.csv")
 create_image_from_csv("trash_data_scaled_interpolated.csv", "machin.png")
-
 build_image_map("trash_data_scaled.csv")
+
+
+## operation on map matrix
+map_matrix = init_grid_matrix(corr_mat)
+get_neighbour(5, map_matrix)
+score = compute_matrix_score(corr_mat, map_matrix)
